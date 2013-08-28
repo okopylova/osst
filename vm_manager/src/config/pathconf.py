e9d5aee4046be251061da429b307a73b2644f0e6
Olga Kopylova <okopylova@mirantis.com>
2013-08-28 20:01:57 +0300

    Remove old project structure

--- vm_manager/src/config/pathconf.py
+++
@@ -1,9 +0,0 @@
-"""This module contains variables for vm_manager path configurations"""
-
-from os.path import expanduser, join
-
-driver = 'qemu:///system'
-home = expanduser('~')
-base_disk_path = join(home, 'vm_disks')
-base_vm_img = join(home, 'osst/vm_manager/data/base_vm_disk.img')
-vm_conf_templ_path = join(home, 'osst/vm_manager/src/config/vm_conf_templ.xml')
